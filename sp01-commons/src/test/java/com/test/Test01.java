337643e7fc218bebdd05ea1729686ab4f177d47f
Administrator <Administrator@XROG98XNEE1N4Z3>
2019-11-01 11:18:09 +0800

    1

---
+++ sp01-commons/src/test/java/com/test/Test01.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		String a="123";
+		String b="123";
+		System.out.println(a==b);
+		System.out.println(a.equals(b));
+	}
+}
